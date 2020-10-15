c1160f846b7afcfa069455059a572c6b67d1cd1f
alexandre-roa <alexandre.z.roa@gmail.com>
2020-10-15 14:58:21 -0300

    fix git merge

--- backend/src/config/format.ts
+++ backend/src/config/format.ts
@@ -1,9 +1,5 @@
 import { ObjectID } from 'mongodb';
 export default {
-<<<<<<< HEAD
   ObjectIDToId: (idToFormat: ObjectID) =>
     JSON.stringify(idToFormat).replace(/"/g, ''),
-=======
-  id: (idToFormat: ObjectID) => JSON.stringify(idToFormat).replace(/"/g, ''),
->>>>>>> ee8ed3c59a7e831d45b585f58c8b38bc0e934fb6
 };
