d6cc8f22560eb5c49ccff8c7880271bc88fc5508
alexandre-roa <alexandre.z.roa@gmail.com>
2020-10-19 10:59:19 -0300

    Component - [ Remove Scale Button Style ]

--- frontend/src/components/Button/styles.ts
+++ frontend/src/components/Button/styles.ts
@@ -10,11 +10,10 @@ export const Container = styled.button`
   width: 100%;
   font-weight: 500;
   margin-top: 16px;
-  transition: background-color ease-in-out 0.2s;
+  transition: background-color 0.3s;
 
   &:hover {
     background: #0dc949;
     box-shadow: inset 0 0 10px #ffff;
-    transform: scale(1.1);
   }
 `;
