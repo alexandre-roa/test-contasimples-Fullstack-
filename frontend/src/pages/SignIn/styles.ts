21ec2d780624ca8bcd97108af09f24377f8a5162
alexandre-roa <alexandre.z.roa@gmail.com>
2020-10-19 12:08:06 -0300

    Pages - [ Create Sign-in Page ]

---
+++ frontend/src/pages/SignIn/styles.ts
@@ -0,0 +1,105 @@
+import styled, { keyframes } from 'styled-components';
+import { shade } from 'polished';
+
+import signInBackgroundImg from '../../assets/sign_in_background.jpg';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  max-width: 700px;
+`;
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromLeft} 1s;
+
+  img {
+    width: 250px;
+    padding: 16px;
+    margin-bottom: 24px;
+  }
+
+  h2 {
+    strong {
+      color: #000;
+      font-weight: 700;
+    }
+  }
+
+  p {
+    color: #666360;
+  }
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #000;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#000')};
+      }
+    }
+  }
+
+  > a {
+    color: #45b54a;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 16px;
+    }
+
+    &:hover {
+      color: #0dc949;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
