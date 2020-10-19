15c270b1976f4880fafe728bec3a722fbb10968f
alexandre-roa <alexandre.z.roa@gmail.com>
2020-10-19 10:34:35 -0300

    Authentication hooks

---
+++ frontend/src/hooks/auth.tsx
@@ -0,0 +1,93 @@
+import React, { createContext, useCallback, useState, useContext } from 'react';
+import api from '../services/api';
+
+interface IUser {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface IAuthState {
+  token: string;
+  user: IUser;
+}
+
+interface ISignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface IAuthContextData {
+  user: IUser;
+  signIn(credentials: ISignInCredentials): Promise<void>;
+  signOut(): void;
+  updateUser(user: IUser): void;
+}
+
+const AuthContext = createContext<IAuthContextData>({} as IAuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<IAuthState>(() => {
+    const token = localStorage.getItem('@ContaSimples:token');
+    const user = localStorage.getItem('@ContaSimples:user');
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as IAuthState;
+  });
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@ContaSimples:token');
+    localStorage.removeItem('@ContaSimples:user');
+
+    setData({} as IAuthState);
+  }, []);
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@ContaSimples:token', token);
+    localStorage.setItem('@ContaSimples:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
+
+    setData({ token, user });
+  }, []);
+
+  const updateUser = useCallback(
+    (user: IUser) => {
+      localStorage.setItem('@ContaSimples:user', JSON.stringify(user));
+
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [setData, data.token],
+  );
+
+  return (
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): IAuthContextData {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
