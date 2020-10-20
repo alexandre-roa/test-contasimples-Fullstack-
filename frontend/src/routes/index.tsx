5e905485402b8053a38837d88702e0221fcca328
alexandre-roa <alexandre.z.roa@gmail.com>
2020-10-20 19:50:13 -0300

    Route - [ Create CreditCard Page ]

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -9,6 +9,7 @@ import SignUp from '../pages/SignUp';
 // import ResetPassword from '../pages/ResetPassword';
 
 import Dashboard from '../pages/Dashboard';
+import CreditCard from '../pages/CreditCards';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -18,6 +19,7 @@ const Routes: React.FC = () => (
     <Route path="/reset-password" component={ResetPassword} /> */}
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
+    <Route path="/credit-card" component={CreditCard} isPrivate />
   </Switch>
 );
 
