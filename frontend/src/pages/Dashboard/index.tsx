f44dfdff8ffd2c8ca7eaf029c34080c13844bb1d
alexandre-roa <alexandre.z.roa@gmail.com>
2020-10-20 13:51:52 -0300

    Dashboard - [ Get all transactions and by type ]

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,11 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo, useCallback } from 'react';
 
 import formatValue from '../../utils/formatValue';
 import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
 import AsideMenu from '../../components/AsideMenu';
 
-import { Container, Content } from './styles';
+import credit from '../../assets/credit.svg';
+import debit from '../../assets/debit.svg';
+import total from '../../assets/total.svg';
+
+import {
+  Container,
+  Content,
+  CardContainer,
+  Card,
+  TableContainer,
+} from './styles';
 
 interface ITransaction {
   id: string;
@@ -14,6 +24,8 @@ interface ITransaction {
   type: 'credit' | 'debit';
   user_id: string;
   final_card: number;
+  formattedValue?: string;
+  formattedDate?: string;
   transaction_type: string;
   transaction_description: string;
   establishment: string;
@@ -21,40 +33,171 @@ interface ITransaction {
   created_at: string;
 }
 
+interface ICard {
+  id: string;
+  label_name: string;
+  card_limit: number;
+  card_number: number;
+  final_card_number: number;
+  due_date: Date;
+  status: string;
+  cvv: number;
+  user_id: string;
+  created_at: string;
+  transactions: ITransaction[];
+}
+
+interface ITypeTransactions {
+  title: string;
+  value: number;
+  type: string;
+}
+
+interface IBankData {
+  user_id: string;
+  user: string;
+  company: string;
+  cnpj: number;
+  balance: number;
+}
+
 const Dashboard: React.FC = () => {
   const { user } = useAuth();
+  const [cards, setCards] = useState<ICard[]>([]);
   const [transactions, setTransactions] = useState<ITransaction[]>([]);
+  const [creditTransactions, setCreditTransactions] = useState<string>('');
+  const [debitTransactions, setDebitTransactions] = useState<string>('');
+  const [bankData, setBankData] = useState<IBankData>({} as IBankData);
 
   useEffect(() => {
-    async function loadTransactions(): Promise<void> {
+    async function loadCreditTransactions(): Promise<void> {
       try {
         const response = await api.get(`transactions/${user.id}`);
 
-        const transactionsFormatted = response.data.map(
-          (transaction: ITransaction) => ({
-            ...transaction,
-            formattedValue: formatValue(transaction.value),
-            formattedDate: new Date(transaction.created_at).toLocaleDateString(
-              'pt-br',
-            ),
-          }),
-        );
-
-        setTransactions(transactionsFormatted);
+        setTransactions(response.data);
       } catch (err) {
         console.log(err);
       }
     }
+    loadCreditTransactions();
+  }, []);
 
-    loadTransactions();
-  }, [user.id]);
+  useEffect(() => {
+    async function loadBankData(): Promise<void> {
+      try {
+        const response = await api.get(`/users/balance/${user.id}`);
 
-  console.log(transactions);
+        setBankData(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    loadBankData();
+  }, []);
+
+  useMemo(async () => {
+    const debit = await api.get(`transactions/${user.id}/debit`);
+
+    const debitValues = debit.data.map(
+      (transaction: ITransaction) => transaction.value,
+    );
+    const formatedDebitValues = debitValues.reduce(
+      (acc: number, cur: number) => acc + cur,
+    );
+
+    setDebitTransactions(formatValue(formatedDebitValues));
+
+    const credit = await api.get(`transactions/${user.id}/credit`);
+
+    const creditValues = credit.data.map(
+      (transaction: ITransaction) => transaction.value,
+    );
+    const formatedCreditValues = creditValues.reduce(
+      (acc: number, cur: number) => acc + cur,
+    );
+
+    setCreditTransactions(formatValue(formatedCreditValues));
+  }, [transactions]);
+
+  const FormatedTransactions = useMemo(() => {
+    if (transactions.length === 0) {
+      return [];
+    }
+    const formatted = transactions.map(transaction => ({
+      ...transaction,
+      formattedValue: formatValue(transaction.value),
+      formattedDate: new Date(transaction.created_at).toLocaleDateString(
+        'pt-br',
+      ),
+    }));
+
+    return formatted;
+  }, [transactions]);
+
+  const handleTransactionChange = useCallback(
+    async (transactionType: string) => {
+      const response = await api.get(
+        `transactions/${user.id}/${transactionType}`,
+      );
+      setTransactions(response.data);
+    },
+    [transactions],
+  );
+
   return (
     <Container>
       <AsideMenu />
       <Content>
-        <h1>Content</h1>
+        <CardContainer>
+          <Card onClick={() => handleTransactionChange('credit')}>
+            <header>
+              <p>Entradas</p>
+            </header>
+            <h1 data-testid="balance-income">{creditTransactions}</h1>
+            <img src={credit} alt="Income" />
+          </Card>
+          <Card outcome onClick={() => handleTransactionChange('debit')}>
+            <header>
+              <p>Saídas</p>
+            </header>
+            <h1 data-testid="balance-outcome">{debitTransactions}</h1>
+            <img src={debit} alt="Outcome" />
+          </Card>
+        </CardContainer>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            {FormatedTransactions &&
+              FormatedTransactions.map((transaction: ITransaction) => (
+                <tbody key={transaction.id}>
+                  <tr>
+                    <td className="title">{transaction.title}</td>
+
+                    {transaction.type === 'credit' ? (
+                      <td className="income">{transaction.formattedValue}</td>
+                    ) : (
+                      <td className="outcome">
+                        - {transaction.formattedValue}
+                      </td>
+                    )}
+
+                    <td>{transaction.transaction_description}</td>
+                    <td>{transaction.formattedDate}</td>
+                    <td></td>
+                  </tr>
+                </tbody>
+              ))}
+          </table>
+        </TableContainer>
       </Content>
     </Container>
   );
--- frontend/src/pages/Dashboard/styles.ts
+++ frontend/src/pages/Dashboard/styles.ts
@@ -1,4 +1,10 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
+
+interface CardProps {
+  outcome?: boolean;
+  total?: boolean;
+}
 
 export const Container = styled.div`
   display: flex;
@@ -7,4 +13,105 @@ export const Container = styled.div`
 export const Content = styled.div`
   display: flex;
   flex: 1;
+  flex-direction: column;
+`;
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+`;
+
+export const CardContainer = styled.section`
+  display: flex;
+  padding: 32px 42px;
+  height: 20vh;
+`;
+
+export const Card = styled.button`
+  align-items: center;
+  display: flex;
+  flex-direction: ${({ total }: CardProps): string =>
+    total ? 'column' : 'row'}row;
+  background: ${({ total }: CardProps): string => (total ? '#0dc949' : '#fff')};
+  padding: 22px 32px;
+  border-radius: 5px;
+  border: ${({ outcome }: CardProps): string =>
+    outcome ? '1px solid #e83f5b' : '1px solid #0dc949'};
+  color: ${({ total }: CardProps): string => (total ? '#fff' : '#363F5F')};
+  margin-right: 24px;
+  transition: 0.2s ease-in-out;
+
+  &:hover {
+    box-shadow: ${({ outcome }: CardProps): string =>
+      outcome
+        ? '0px 8px 24px 0px rgba(232,63,91,0.5)'
+        : '0px 8px 24px 0px rgba(13, 201, 73, 0.5)'};
+
+    transform: scale(1.2);
+  }
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    p {
+      font-size: 16px;
+    }
+  }
+
+  h1 {
+    margin-top: 14px;
+    font-size: 36px;
+    font-weight: normal;
+    line-height: 54px;
+    padding: 0 24px;
+  }
+`;
+
+export const TableContainer = styled.section`
+  margin-top: 64px;
+
+  table {
+    width: 100%;
+    border-spacing: 0 8px;
+
+    th {
+      color: #969cb3;
+      font-weight: normal;
+      padding: 20px 32px;
+      text-align: left;
+      font-size: 16px;
+      line-height: 24px;
+    }
+
+    td {
+      padding: 20px 32px;
+      border: 0;
+      background: #fff;
+      font-size: 16px;
+      font-weight: normal;
+      color: #969cb3;
+
+      &.title {
+        color: #363f5f;
+      }
+
+      &.income {
+        color: #12a454;
+      }
+
+      &.outcome {
+        color: #e83f5b;
+      }
+    }
+
+    td:first-child {
+      border-radius: 8px 0 0 8px;
+    }
+
+    td:last-child {
+      border-radius: 0 8px 8px 0;
+    }
+  }
 `;
