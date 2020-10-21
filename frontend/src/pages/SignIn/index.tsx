9566eb6eb33b618260b75549572ab57df6138184
alexandre-roa <alexandre.z.roa@gmail.com>
2020-10-21 14:37:00 -0300

    Page - [Remove link to forgot password]

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -91,8 +91,6 @@ const SignIn: React.FC = () => {
             />
 
             <Button type="submit">Entrar</Button>
-
-            <Link to="/forgot-password">Esqueci minha senha</Link>
           </Form>
 
           <Link to="/signup">
